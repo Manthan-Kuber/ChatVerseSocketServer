f2ecd9da39d447dcf1bd570faa292acf6a4ceccd
Manthan Kuber <manthankuber873@gmail.com>
2022-11-28 18:23:20 +0530

    feat: create express http server

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,31 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app: Application = express();
+
+app.use(cors());
+
+const httpServer = http.createServer(app);
+
+app.use(express.json());
+
 const port = process.env.HTTP_SERVER_PORT;
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hi");
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+  },
 });
 
-app.listen(port, () => {
+io.on("connection",(socket) => {
+  console.log(`User Connected with id: ${socket.id}`)
+} )
+
+httpServer.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });

21e2fd3f64f38f20ca4ad2941560ac6fa64fd0e0
Manthan Kuber <manthankuber873@gmail.com>
2023-02-03 19:58:08 +0530

    feat: prisma setup for server

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,15 +1,19 @@
-import { z, ZodFormattedError } from "zod";
+import { z, type ZodFormattedError } from "zod";
 import dotenv from "dotenv";
 dotenv.config();
 
 const envSchema = z.object({
   HTTP_SERVER_PORT: z.string(),
   CLIENT_URL: z.string().url(),
+  DATABASE_URL: z.string(),
+  NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
 const __env = {
   HTTP_SERVER_PORT: process.env.HTTP_SERVER_PORT,
   CLIENT_URL: process.env.CLIENT_URL,
+  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV,
 };
 
 const _env = envSchema.safeParse(__env);
